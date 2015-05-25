cd31d9a343d980433aa049f872efc60637560ac5
Andreas Gillström <andreasgillstrom@gmail.com>
2015-05-25 19:21:45 +0200

    camelCase

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 var execFile = require('child_process').execFile;
 var plist = require('simple-plist');
+var changeCase = require('change-case');
 
 module.exports = function (cb) {
 	if (process.platform !== 'darwin') {
 		throw new Error('Only OS X systems are supported');
 	}
 
-	var arr;
 	var cmd = 'ioreg';
 	var args = [
 		'-n',
@@ -27,6 +27,29 @@ module.exports = function (cb) {
 			return;
 		}
 
-		cb(null, plist.parse(res)[0]);
+		var obj = {};
+		var keys = Object.keys(Object(plist.parse(res)[0]));
+
+		for (var i = 0; i < keys.length; i++) {
+			obj[changeCase.lowerCaseFirst(keys[i])] = plist.parse(res)[0][keys[i]];
+		}
+
+		if (typeof obj.legacyBatteryInfo === 'object') {
+			var l = {};
+			var keys = Object.keys(Object(obj.legacyBatteryInfo));
+
+			for (var i = 0; i < keys.length; i++) {
+				if (keys[i].indexOf(' ') !== -1) {
+					l[changeCase.camelCase(keys[i])] = obj.legacyBatteryInfo[keys[i]];
+					continue;
+				}
+
+				l[changeCase.lowerCaseFirst(keys[i])] = obj.legacyBatteryInfo[keys[i]];
+			}
+
+			obj.legacyBatteryInfo = l;
+		}
+
+		cb(null, obj);
 	});
 };
